986e4f70f7e8f3fd42ab4bc19eddc1774acce475
tokatu4561 <ryobsk4561@gmail.com>
2022-05-26 06:24:00 -0700

    react-route-dom 6->5

--- react-query-todo/src/App.tsx
+++ react-query-todo/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="">
-    </div>
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono">
+      <BrowserRouter>
+        <Switch> 
+        </Switch>
+      </BrowserRouter>
+  </div>
   );
 }
 
